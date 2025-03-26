9bf65dc27ad54cf7bb509b417096ae35642ffeeb
XXAdityaXX <1032200566@mitwpu.edu.in>
2025-03-26 09:39:50 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, PaaS Deployment!"})
+    return "Hello, this is your Flask web service deployed on Render!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
